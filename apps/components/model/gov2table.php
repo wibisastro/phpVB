f0a42060174b6f9bd2d90ebe191d7f7172fdc6ad
Wibisono Sastrodiwiryo <wibi.sastro@gmail.com>
2026-03-03 12:49:16 +0700

    bug fix table json

--- apps/components/model/gov2table.php
+++ apps/components/model/gov2table.php
@@ -5,7 +5,7 @@ class gov2table extends \Gov2lib\dsnSource {
 		$this->templateDir=__DIR__."/../view";
         $path=explode("\\",__CLASS__);
         $this->className=$path[sizeof($path)-1];
-        $GLOBALS['vueData']['geturl']='/gov2table';
+        $GLOBALS['vueData']['geturl']='/components/gov2table';
         $GLOBALS['vueData']['isTableActive']='true';
         $GLOBALS['vueData']['records']=0;
         $GLOBALS['vueData']['interval']=array(10,20,50,100);
@@ -21,26 +21,33 @@ class gov2table extends \Gov2lib\dsnSource {
         $GLOBALS['vueData']['interval']=array(1,2,5,10);
 	}
     
+    private function loadJson () {
+        $path = __DIR__ . '/../json/table.json';
+        $all  = json_decode(file_get_contents($path), true);
+        return $all ?: [];
+    }
+
     function count () {
-		list($_link_id,$_name)=$this->connectDB();
-		$query="SELECT count(id) as totalRecord FROM ".$this->tbl->tematik;
-		$buffer=$this->queryDB($_name, $query,$_link_id);
-		$result=mysqli_fetch_object($buffer);
-	return $result;
-	}
-    
+        $all    = $this->loadJson();
+        $result = new \stdClass();
+        $result->totalRecord = count($all);
+        return $result;
+    }
+
     function browse ($scroll=0) {
-        $scrolled=$this->scroll($scroll);
-		list($_link_id,$_name)=$this->connectDB();
-		$query="SELECT * FROM ".$this->tbl->tematik." ORDER BY id LIMIT $scrolled";
-		$daftar = $this->queryDB($_name, $query,$_link_id);
-        $c=1;$page=1;
-		while ($buffer = mysqli_fetch_object($daftar)) {
-            $result[$c]=$buffer;
-            $c++;
-        }        
-	return $result;
-	}
+        $all      = $this->loadJson();
+        $rows     = array_values($all);
+        $interval = $this->scrollInterval ?: 4;
+        $scroll   = max(1, (int)$scroll);
+        $offset   = ($scroll - 1) * $interval;
+        $slice    = array_slice($rows, $offset, $interval);
+        if (empty($slice)) return ['data' => 'empty'];
+        $result = [];
+        foreach ($slice as $i => $row) {
+            $result[$i + 1] = $row;
+        }
+        return $result;
+    }
 
     function read ($id) {
         $id+=0;
