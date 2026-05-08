f3417ace4e34bd79e8fef25f9b9a6c7cbd3a2d84
Wibisono Sastrodiwiryo <wibi@alumni.ui.ac.id>
2026-05-08 21:00:29 +0700

    fix a bug

--- apps/components/model/gov2nav.php
+++ apps/components/model/gov2nav.php
@@ -17,15 +17,29 @@ class gov2nav extends \Gov2lib\document {
 
 	function setDefaultNav ($_menuFile="") {
         global $pageID,$config,$self;
+        // Accumulate menu data (boleh dipanggil berkali-kali untuk multi-app)
         $this->menus=$this->menubar($pageID,$_menuFile);
-        $this->sidebar('gov2navMenu.html');
-        $this->content('gov2navBreadcrumb.html');
+        // Register sidebar template hanya 1x meskipun method dipanggil berkali-kali
+        if (!($this->_sidebarRegistered ?? false)) {
+            $this->sidebar('gov2navMenu.html');
+            $this->_sidebarRegistered = true;
+        }
+        // Register breadcrumb template hanya 1x
+        if (!($this->_breadcrumbRegistered ?? false)) {
+            $this->content('gov2navBreadcrumb.html');
+            $this->_breadcrumbRegistered = true;
+        }
         $GLOBALS['vueData']['pathurl']=rtrim($config->webroot."/components/gov2nav/breadcrumb/$pageID/".$self->className."/".str_replace(".xml","",$_menuFile), '/');
 	}
 
     function setCustomNav ($_menuFile="") {
         global $pageID,$config,$self;
-        $this->sidebar('gov2navMenuCustom.html');
+        // setCustomNav: Vue fetch async, tidak ada akumulasi data di sini.
+        // Template hanya perlu register 1x.
+        if (!($this->_sidebarRegistered ?? false)) {
+            $this->sidebar('gov2navMenuCustom.html');
+            $this->_sidebarRegistered = true;
+        }
         $GLOBALS['vueData']['pathurl']=rtrim($config->webroot."/components/gov2nav/breadcrumb/$pageID/".$self->className."/".str_replace(".xml","",$_menuFile), '/');
     }
 	
